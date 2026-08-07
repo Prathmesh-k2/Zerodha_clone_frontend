d5fb82e67f4a8ec4a9f189a12fb8f0dcfcecf707
Prathmesh Kokare <prathmeshkokare009@gmail.com>
2026-08-07 15:25:22 +0530

    Fix Hero.js: replace fake anchor tags with button elements

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     <section className="container-fluid" id="supportHero">
       <div className="p-5 " id="supportWrapper">
         <h4>Support Portal</h4>
-        <a href="/support">Track Tickets</a>
+        <button className="support-link-btn">Track Tickets</button>
       </div>
       <div className="row p-5 m-3">
         <div className="col-6 p-3">
@@ -14,19 +14,19 @@ function Hero() {
           </h1>
           <input placeholder="Eg. how do I activate F&O" />
           <br />
-          <a href="/support/account-opening">Track account opening</a>
-          <a href="/support/segment-activation">Track segment activation</a>
-          <a href="/support/intraday-margins">Intraday margins</a>
-          <a href="/support/kite-manual">Kite user manual</a>
+          <button className="support-link-btn">Track account opening</button>
+          <button className="support-link-btn">Track segment activation</button>
+          <button className="support-link-btn">Intraday margins</button>
+          <button className="support-link-btn">Kite user manual</button>
         </div>
         <div className="col-6 p-3">
           <h1 className="fs-3">Featured</h1>
           <ol>
             <li>
-              <a href="/support/takeovers-delisting">Current Takeovers and Delisting - January 2024</a>
+              <button className="support-link-btn">Current Takeovers and Delisting - January 2024</button>
             </li>
             <li>
-              <a href="/support/intraday-leverages">Latest Intraday leverages - MIS & CO</a>
+              <button className="support-link-btn">Latest Intraday leverages - MIS &amp; CO</button>
             </li>
           </ol>
         </div>
